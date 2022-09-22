8ac2c91a45c2bce496e59b36e47e313bb364943d
David Sullivan <dcsulliv@gmail.com>
2022-09-22 17:52:40 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RestaurantRanterMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
