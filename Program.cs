118caf49b7881b734e56e83b86013342e7ce8cf2
David Sullivan <dcsulliv@gmail.com>
2022-09-22 19:49:36 -0400

    RR 0.08 Database Context

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<RestaurantDbContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
